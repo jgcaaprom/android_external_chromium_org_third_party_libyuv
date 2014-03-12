329ac9dc4e3cd2d35cd46577d257757a540d30b4
Android Chromium Automerger <chromium-automerger@android>
2014-03-12 00:41:29 +0000

    Update makefiles after merge of Chromium at 256368
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: Ic3e16f4b7c6fe2596ca8718024cea1427708998b

--- libyuv.target.linux-x86.mk
+++ libyuv.target.linux-x86.mk
@@ -68,11 +68,10 @@ MY_CFLAGS_Debug := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
@@ -160,11 +159,10 @@ MY_CFLAGS_Release := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
