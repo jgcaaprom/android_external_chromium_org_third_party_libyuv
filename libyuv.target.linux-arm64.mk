7d85f8a2756eb80ee5c893506cd6c877fb902038
Android Chromium Automerger <chromium-automerger@android>
2014-06-07 09:08:36 +0000

    Update makefiles after merge of Chromium at 275661
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: If7f7577c6abb89451ab276180a6e4837570100c2

--- libyuv.target.linux-arm64.mk
+++ libyuv.target.linux-arm64.mk
@@ -254,7 +254,6 @@ LOCAL_LDFLAGS_Debug := \
 	-fPIC \
 	-nostdlib \
 	-Wl,--no-undefined \
-	-Wl,--exclude-libs=ALL \
 	-Wl,--warn-shared-textrel \
 	-Wl,-O1 \
 	-Wl,--as-needed
@@ -268,7 +267,6 @@ LOCAL_LDFLAGS_Release := \
 	-fPIC \
 	-nostdlib \
 	-Wl,--no-undefined \
-	-Wl,--exclude-libs=ALL \
 	-Wl,-O1 \
 	-Wl,--as-needed \
 	-Wl,--gc-sections \
